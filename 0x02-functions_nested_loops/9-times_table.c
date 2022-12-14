1b1d0abcc2291f21ab6ce7698e029d17dabca581
Maxilink <miraculux35@gmail.com>
2022-12-14 08:12:02 -0800

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,10 +8,38 @@
  */
 
 
-
-
-int main(void)
+void times_table(void)
 {
-	times_table();
-	return (0);
+	int row;
+	int column;
+	int product;
+
+	for (row = 0; row <= 9; row++)
+	{
+		for (column = 0; column <= 9; column++)
+		{
+			product = (row * column);
+
+			if (column == 0)
+			{
+				_putchar('0' + product);
+			}
+			else if (product <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + product);
+			}
+			else if (product > 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + (product / 10));
+				_putchar('0' + (product % 10));
+
+			}
+		}
+		_putchar('\n');
+	}
 }
