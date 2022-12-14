64c260fe134444b77a6aa8762e69cd346af94973
Maxilink <miraculux35@gmail.com>
2022-12-14 04:30:32 -0800

    Tast 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints all alphabets in lowercase 10 times.
+ * Return: On success 1.
+ */
+
+
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int counter;
+
+
+	for (counter = 1; counter <= 10; counter++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+
+		_putchar('\n');
+
+	}
+
+}
