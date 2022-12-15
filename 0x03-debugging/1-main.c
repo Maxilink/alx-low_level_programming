00448bb30b5c8e1451aa9641761a50344e797302
Maxilink <miraculux35@gmail.com>
2022-12-15 08:36:37 -0800

    Task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,8 +5,6 @@
  * Return: 0
  */
 
-
-
 int main(void)
 {
 	int i;
@@ -15,14 +13,12 @@ int main(void)
 
 	i = 0;
 
-
 	/*while (i < 10)*/
-	/*{*/
-	/*putchar(i);*/
-	/*}*/
+	 /*{*/
+	 /*putchar(i);*/
+	 /*}*/
 
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
-
 }
