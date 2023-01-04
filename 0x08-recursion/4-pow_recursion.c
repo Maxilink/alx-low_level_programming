49f63badf068474c491db8338b59d8ab8572e272
Maxilink <miraculux35@gmail.com>
2023-01-04 09:44:32 -0800

    Task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -11,13 +11,13 @@
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
-	{
 		return (-1);
-	}
-	if (y == 0)
-	{
+
+	if (x == 0)
+		return (0);
+
+	if (x == 1 || y == 0)
 		return (1);
-	}
-	printf("%d\n", x * x);
+
 	return (x * _pow_recursion(x, y - 1));
 }
